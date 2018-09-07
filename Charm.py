81de4ebf03f05c6b590c1746f9be3423e174c890
oklodhi <okhanlodhi@gmail.com>
2018-09-07 16:54:05 -0500

    Commit completed web crawler script

--- Charm.py
+++ Charm.py
@@ -1,17 +1,40 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pymysql.cursors
+#import mysql.connector
 
-# WEBPAGE CONNECTION
-url = urlopen ("http://www.officialcharts.com/charts/singles-chart/")
+# CONNECT TO OFFICIALCHARTS WEB PAGE
+url = urlopen("http://www.officialcharts.com/charts/singles-chart/")
 
 # GRAB ARTIST AND TITLE
 bsObj = BeautifulSoup(url, features="lxml")
-recordList = bsObj.findAll("div", {"class" : "title-artist",})
+recordList = bsObj.findAll("div", {"class": "title-artist", })
+recordRank = bsObj.findAll("span", {"class": "position", })
 
 # ITERATE OVER RECORDLIST TO GRAB ARTIST AND TITLE
 for record in recordList:
-     title = record.find("div", {"class": "title",}).get_text().strip()
-     artist = record.find("div", {"class": "artist"}).get_text().strip()
-     print (artist + ': ' + title)
+    title = record.find("div", {"class": "title"}).get_text().strip()
+    artist = record.find("div", {"class": "artist"}).get_text().strip()
+    print(artist + ': ' + title)
 
+# MYSQL DATABASE CONNECTION
+mydb = pymysql.connect(
+    host="localhost",
+    user="root",
+    passwd="password",
+    database="topCharts",
+    cursorclass= pymysql.cursors.DictCursor)
+
+# TRY TO INSERT ITEMS INTO DATABASE TABLE 'ARTIST_SONG'
+try:
+    with mydb.cursor() as cursor:
+        for record in recordList:
+            title = record.find("div", {"class": "title"}).get_text().strip()
+            artist = record.find("div", {"class": "artist"}).get_text().strip()
+            sql = "INSERT INTO artist_song (artist, song) VALUEs (%s, %s)"
+            cursor.execute(sql, (artist, title))
+        mydb.commit()
+finally:
+    mydb.close()
+
+print("\nRecord inserted.")
