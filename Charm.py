fa67ea4b73a1f254023532e37aeea955236a3407
oklodhi <okhanlodhi@gmail.com>
2018-09-07 22:55:28 -0500

    Commit changes

--- Charm.py
+++ Charm.py
@@ -1,40 +1,41 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pymysql.cursors
-#import mysql.connector
 
-# CONNECT TO OFFICIALCHARTS WEB PAGE
-url = urlopen("http://www.officialcharts.com/charts/singles-chart/")
 
-# GRAB ARTIST AND TITLE
-bsObj = BeautifulSoup(url, features="lxml")
-recordList = bsObj.findAll("div", {"class": "title-artist", })
-recordRank = bsObj.findAll("span", {"class": "position", })
+def run():
+    # CONNECT TO OFFICIALCHARTS WEB PAGE
+    url = urlopen("http://www.officialcharts.com/charts/singles-chart/")
 
-# ITERATE OVER RECORDLIST TO GRAB ARTIST AND TITLE
-for record in recordList:
-    title = record.find("div", {"class": "title"}).get_text().strip()
-    artist = record.find("div", {"class": "artist"}).get_text().strip()
-    print(artist + ': ' + title)
+    # GRAB ARTIST AND TITLE
+    bsObj = BeautifulSoup(url, features="lxml")
+    recordList = bsObj.findAll("div", {"class": "title-artist", })
+    recordRank = bsObj.findAll("span", {"class": "position", })
 
-# MYSQL DATABASE CONNECTION
-mydb = pymysql.connect(
-    host="localhost",
-    user="root",
-    passwd="password",
-    database="topCharts",
-    cursorclass= pymysql.cursors.DictCursor)
+    # ITERATE OVER RECORDLIST TO GRAB ARTIST AND TITLE
+    for record in recordList:
+        title = record.find("div", {"class": "title"}).get_text().strip()
+        artist = record.find("div", {"class": "artist"}).get_text().strip()
+        print(artist + ': ' + title)
 
-# TRY TO INSERT ITEMS INTO DATABASE TABLE 'ARTIST_SONG'
-try:
-    with mydb.cursor() as cursor:
-        for record in recordList:
-            title = record.find("div", {"class": "title"}).get_text().strip()
-            artist = record.find("div", {"class": "artist"}).get_text().strip()
-            sql = "INSERT INTO artist_song (artist, song) VALUEs (%s, %s)"
-            cursor.execute(sql, (artist, title))
-        mydb.commit()
-finally:
-    mydb.close()
+    # MYSQL DATABASE CONNECTION
+    mydb = pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="password",
+        database="topCharts",
+        cursorclass=pymysql.cursors.DictCursor)
 
-print("\nRecord inserted.")
+    # TRY TO INSERT ITEMS INTO DATABASE TABLE 'ARTIST_SONG'
+    try:
+        with mydb.cursor() as cursor:
+            for record in recordList:
+                title = record.find("div", {"class": "title"}).get_text().strip()
+                artist = record.find("div", {"class": "artist"}).get_text().strip()
+                sql = "INSERT INTO artist_song (artist, song) VALUEs (%s, %s)"
+                cursor.execute(sql, (artist, title))
+            mydb.commit()
+    finally:
+        mydb.close()
+
+    print("\nRecord inserted.")
--- webApp.py
+++ webApp.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import pymysql.cursors
+import Charm
 
 # FLASK APP NAME
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+    Charm.run()
     # MYSQL DATABASE CONNECTION
     mydb = pymysql.connect(
         host="localhost",
@@ -24,7 +26,9 @@ def main():
             query = "SELECT * from artist_song"
             cur.execute(query)
             # FETCH THE RESULTS AS A LIST
-            data = cur.fetchall()
+            data = cur.fetchmany(1)
+            truncate = "TRUNCATE TABLE artist_song"
+            #cur.execute(truncate)
             mydb.close()
             return render_template('index.html', data=data)
     except Exception as e:
@@ -33,3 +37,6 @@ def main():
 # MAIN PROGRAM
 if __name__ == "__main__":
     app.run()
+
+    #cur.execute(truncate)
+    #mydb.close()
